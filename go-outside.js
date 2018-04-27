05e7f1d6aec5b8698daa5b1d71772dc36b2967b5
Abraham Prince <abeprincec@gmail.com>
2018-04-27 17:43:11 -0400

    add conditionals exercises

---
+++ go-outside.js
@@ -0,0 +1,21 @@
+var raining = true;
+var cold = true;
+
+if (cold) {
+  console.log('Make sure you pick out a scarf!');
+} else {
+  console.log("Now you're ready to go outside!");
+}
+
+
+if (isCitizen && age > 18) {
+    console.log("You are eligible to vote.");
+  }
+
+  if (temperature < -40 || temperature > 40) {
+    console.log("Maybe going outside isn't such a great idea…");
+  }
+
+  if (!raining) {
+    console.log("Leave your umbrella at home!");
+  }
